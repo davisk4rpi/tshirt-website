7adee8dd3d226d2eee7edfb9b2ef6f1a815dfc31
davisk4rpi <davisk4rpi@gmail.com>
2017-09-23 10:14:40 -0700

    add route files

--- routes/shirtRoutes.js
+++ routes/shirtRoutes.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
 const Shirt = mongoose.model('shirt');
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
   app.get('/api/shirts', async (req, res) => {
     const shirts = await Shirt.find({});
     res.send(shirts);
   });
-  app.post('/api/shirts' async (req, res) => {
-    const {values} = req;
-    const shirt  = new Shirt(values);
+  app.post('/api/shirts', requireLogin, async (req, res) => {
+    const { values } = req;
+    const shirt = new Shirt(values);
     try {
       const savedShirt = await shirt.save();
       res.send(savedShirt);
-    } catch(err) {
+    } catch (err) {
       res.status(422).send(err);
     }
   });
