4674212608fc97cc7915fd8506eebdc2e647f9a4
davisk4rpi <davisk4rpi@gmail.com>
2017-09-22 22:41:56 -0700

    add shirt model

---
+++ models/Shirt.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoos;
+
+const ShirtSchema = new Schema({
+  name: {
+    type: String,
+    require: [true, 'Name is Required']
+  },
+  price: {
+    type: String,
+    require: [true, 'Price is Required']
+  },
+  link: {
+    type: String,
+    require: [true, 'Link is Required']
+  },
+  imageURL: {
+    type: String,
+    require: [true, "Image URL is Required"]
+  }
+  description: String
+});
+
+const Shirt = mongoose.model('shirt', ShirtSchema);
