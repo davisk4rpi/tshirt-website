53d9e6e563f9b4a09a976b0afb4b6014e3c8dd41
Kieran <davisk4rpi@gmail.com>
2017-10-15 20:36:01 -0700

    add banner image

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import BannerImg from '../assets/banner-rad-dope.png';
 
 const Banner = () => {
   return (
     <section id="banner">
-      <h1>Dope Shirts</h1>
-      <h2>...for Dope People</h2>
+      <img src={BannerImg} alt="Rad Banner"/>
+      <div id="banner-content">
+        <h1>Dope Shirts</h1>
+        <h2>...for Dope People</h2>
+      </div>
     </section>
   );
 };
